397170cf9592dbd44f5a790e2056db6f13180bf7
Lorenzo Bergami <lorebergam@gmail.com>
2026-04-11 19:25:26 +0200

    small refactor

--- script.js
+++ script.js
@@ -7,31 +7,47 @@ function getRndInteger(min, max) {
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
+const dirs = ["up", "down", "left", "right"];
+
+let game_map;
+
 const djsConnect = DjsConnect(
     process.env.HOST,
     process.env.TOKEN,
     process.env.USER_NAME
 );
 
-const dirs = ["up", "down", "left", "right"];
+djsConnect.onMap(async (width, height, tiles) => {
+    game_map = { width, height, tiles };
+    console.log(`Received Map: w=${game_map.width} h=${game_map.height}`);
+    // console.log(`tiles = ${game_map.tiles}`);
+    for (let i = 0; i < game_map.tiles.length; i++) {
+        const tile = game_map.tiles[i];
+        console.log(`tile${i}: x = ${tile.x} y = ${tile.y} type = ${tile.type}`)
+    }
+});
 
 djsConnect.onYou(me => console.log("Received update for me: " + me.x + ", " + me.y));
 
 async function move() {
     while (true) {
+        if (!game_map) {
+            await delay(500);
+            continue;
+        }
         let r = getRndInteger(0, 3);
         console.log("Trying a move to " + dirs[r]);
 
-        const res = await djsConnect.emitMove(dirs[r]);
-        if (res.hasOwnProperty('x') && res.hasOwnProperty('y')) {
-            console.log("moved to " + res.x + ", " + res.y);
+        const map = await djsConnect.map();
+        const move = await djsConnect.emitMove(dirs[r]);
+        if (move.hasOwnProperty('x') && move.hasOwnProperty('y')) {
+            console.log("moved to " + move.x + ", " + move.y);
         } else {
             console.log("move failed");
         }
 
         await delay(500);
     }
-
 }
 
 move();
